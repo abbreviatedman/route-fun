2d234c71088086a881d40bfdbb4b69269ab3eb38
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-04 07:10:46 -0500

    Finishes project.

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ app.get("/about", (req, res) =>
   res.send("This is a practice app to learn about express routes.")
 );
 
-app.get("/:input", (req, res) => {
-  console.log("req.params: ", req.params);
-  res.send("Our parameter is: " + req.params.input + ".");
-});
+// app.get("/:input", (req, res) => {
+//   console.log("req.params: ", req.params);
+//   res.send("Our parameter is: " + req.params.input + ".");
+// });
 
 app.get("/greet/:name", (req, res) => {
   res.send("Hello " + req.params.name + "!");
 });
 
-// app.get("/querystring", (req) => )
+app.get("/querystring", (req, res) => {
+  let printOut = "";
+  for (const key in req.query) {
+    printOut += key + ": " + req.query[key] + "<br />";
+  }
+
+  res.send("Here's what they sent:<br /><br />" + printOut);
+});
 
 app.listen(8000);
